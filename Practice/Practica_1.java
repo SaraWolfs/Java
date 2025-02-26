e0e7d1d4d84a8ff57f7ea5a774887ec57fc5ebd3
SaraWolfs <lokicroki@gmail.com>
2025-02-26 19:18:33 +0100

    Enunciado de primera practica

---
+++ Practice/Practica_1.java
@@ -0,0 +1,6 @@
+public class Practica_1 {
+    //Secuenciales
+    /*1. Realizar en Java un programa que calcule el cuadrado y el cubo de un número entero dado en el 
+    programa y visualice los resultados. Utiliza el método pow de la librería Math de Java.*/ 
+
+}
